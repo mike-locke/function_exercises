3a99bb4f21e1697638c8db14708125d7370a4322
Dan Shafie <danshafie@gmail.com>
2020-08-12 12:57:05 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,66 @@
+//below are some exercises for things you've learned so far
+
+// 1. write a function that takes in two parameters (that are integers) and returns the larger integer
+
+//  example: function largerInteger(5,6) -> would return 6
+
+function largerInteger(arg1, arg2) {
+  // do logic here
+}
+
+// 2. write a function that takes in one argument (an array) and finds the smallest number
+
+// example: function smallestNum([3,2,6,2,0,7]) -> would return 0
+
+function smallestNum(arr) {
+  // do logic here
+}
+
+// 3. write a function that starts at 0 and loops through 15. if the current number is odd console.log -> odd if the current number is even log -> even
+
+//example: function isOddOrEven() -> loops 0-15 logging odd/even
+
+function isOddOrEven() {
+  // do logic here
+}
+
+// 4. given an array, write a function that adds all the numbers together
+
+//example: function add([1,2,3,4,5]) -> returns 15
+
+function add(arr) {
+  // do logic here
+}
+
+// 5. write a function that removes a specific element from an array
+
+// example: function removeItem([3,4,5],5) -> returns [3,4]
+
+function removeItem(arr, item) {
+  // do logic here
+}
+
+// 6. write a function that removes all false values from an array (youll need to find out what all the false values in javascript are)
+
+// example: function removeFalsey(['dog', 'cat', 0, false,]) -> returns ['dog','cat']
+
+function removeFalsey(arr) {
+  // do logic here
+}
+
+// 7. write a javascript function to sort items in an array
+
+// example: function sortArr([4,5,6,1,0,9,3]) -> [0,1,3,4,5,6,9]
+
+function sortArr(arr) {
+  // do logic here
+}
+
+// 8. write a function that checks if the string passed as a parameter is a palindrome or not (palindrome reads the same forwards as backwards ie: racecar)
+
+// example: function palindrome(racecar) -> returns true
+// example: function palindrome(javascript) -> returns false
+
+function palindrome(str) {
+  // do logic here
+}
